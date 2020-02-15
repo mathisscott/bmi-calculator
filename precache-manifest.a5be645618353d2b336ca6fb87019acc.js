ef010c6e7b5ae7cc8f016068163b3811dbcbab74
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-02-15 18:10:27 +0000

    Deploy GermaVinsmoke/bmi-calculator to github.com/GermaVinsmoke/bmi-calculator.git:gh-pages

---
+++ precache-manifest.a5be645618353d2b336ca6fb87019acc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4af7d9863bd533eee63d097febb2d5bd",
+    "url": "/bmi-calculator/index.html"
+  },
+  {
+    "revision": "8293a912b58224040af6",
+    "url": "/bmi-calculator/static/css/2.ae9e8b99.chunk.css"
+  },
+  {
+    "revision": "6287b54d4f95411a9822",
+    "url": "/bmi-calculator/static/css/main.fce36487.chunk.css"
+  },
+  {
+    "revision": "8293a912b58224040af6",
+    "url": "/bmi-calculator/static/js/2.920416a7.chunk.js"
+  },
+  {
+    "revision": "6287b54d4f95411a9822",
+    "url": "/bmi-calculator/static/js/main.8ee08b84.chunk.js"
+  },
+  {
+    "revision": "9b25b4e690523b173542",
+    "url": "/bmi-calculator/static/js/runtime~main.425d477f.js"
+  }
+]);
